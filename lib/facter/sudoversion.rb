4a000a45fd97cd73cc9524900759f1986503cf9a
ky3mr2 <65817726+ky3mr2@users.noreply.github.com>
2026-03-04 15:00:31 +0000

    Update regex to work for Rust sudo variant (sudo-rs)

--- lib/facter/sudoversion.rb
+++ lib/facter/sudoversion.rb
@@ -8,7 +8,7 @@ Facter.add(:sudoversion) do
   setcode do
     if Facter::Util::Resolution.which('sudo')
       sudoversion = Facter::Util::Resolution.exec('sudo -V 2>&1')
-      %r{^Sudo version ([\w.]+)}.match(sudoversion)[1]
+      %r{^(?:Sudo version|sudo-rs)\s+([\w.]+)}i.match(sudoversion)[1]
     elsif Facter::Util::Resolution.which('rpm')
       Facter::Util::Resolution.exec('rpm -q sudo --qf \'%{VERSION}\'')
     elsif Facter::Util::Resolution.which('dpkg-query')
